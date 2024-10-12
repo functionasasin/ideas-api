5eda6ffa9fa7bc89d8a430bd8e958ade85f79b21
kultrum <marcobasug43@gmail.com>
2024-10-12 19:25:27 +0800

    updated http_exception_handler to handle other exceptions beyond 401 Unauthorized

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from typing import List, Dict, Any
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.exceptions import HTTPException
 from fastapi.exception_handlers import http_exception_handler as default_http_exception_handler
@@ -42,7 +43,7 @@ app.add_exception_handler(InvalidIDException, invalid_id_exception_handler)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return create_error_response(
+    return create_error_response[List[Dict[str, Any]]](
         error_code=ErrorCodes.VALIDATION_ERROR.value,
         message="Validation error",
         details=exc.errors(),
@@ -50,16 +51,28 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     )
 
 @app.exception_handler(HTTPException)
-async def http_exception_handler_401(request: Request, exc: HTTPException):
-    if exc.status_code == 401:
-        return create_error_response(
+async def http_exception_handler(request: Request, exc: HTTPException):
+    if exc.status_code == HTTPStatusCodes.UNAUTHORIZED.value:
+        return create_error_response[None](
             error_code=ErrorCodes.UNAUTHORIZED_ACCESS.value,
             message=exc.detail or "Unauthorized access",
             details=None,
             status_code=exc.status_code
         )
+    elif exc.status_code == HTTPStatusCodes.NOT_FOUND.value:
+        return create_error_response[None](
+            error_code=ErrorCodes.RESOURCE_NOT_FOUND.value,
+            message=exc.detail or "Resource not found",
+            details=None,
+            status_code=exc.status_code
+        )
     else:
-        return await default_http_exception_handler(request, exc)
+        return create_error_response[None](
+            error_code="HTTPException",
+            message=exc.detail or "An error occurred",
+            details=None,
+            status_code=exc.status_code
+        )
 
 # uvicorn src.main:app --reload
 
